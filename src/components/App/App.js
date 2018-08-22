78f82782d50822c3a38b9754b2049b71d7f8fddb
Matt Crouch <matt@mattcrouch.net>
2018-08-22 22:35:35 +0100

    Remove "NotFound" state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Redirect, Switch } from "react-router-dom";
 
 import Header from "../presentational/Header";
 import Home from "../pages/Home";
-import NotFound from "../pages/NotFound";
 import Photo from "../pages/Photo";
 
 import "./styles.css";
@@ -16,7 +15,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/photo/:id" component={Photo} />
-          <Route component={NotFound} />
+          <Redirect to="/" />
         </Switch>
       </div>
     );
