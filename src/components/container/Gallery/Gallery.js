25b1a7a21261992bf2deaa0dcbc16606d6573971
Matt Crouch <matt@mattcrouch.net>
2018-08-25 13:43:48 +0100

    GalleryContainer PropTypes

--- src/components/container/Gallery/Gallery.js
+++ src/components/container/Gallery/Gallery.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import Gallery from "../../presentational/Gallery";
@@ -12,5 +13,14 @@ export class GalleryContainer extends Component {
 export const mapStateToProps = state => ({
   photos: Object.values(state.photos)
 });
+
+GalleryContainer.propTypes = {
+  photos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired
+    })
+  )
+};
 
 export default connect(mapStateToProps)(GalleryContainer);
