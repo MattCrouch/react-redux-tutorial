35e8d7444245f4efebc585f77a8203a5f3a4a01e
Matt Crouch <matt@mattcrouch.net>
2018-08-25 12:41:44 +0100

    Basic reducer set up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { createStore } from "redux";
 import App from "./components/App";
+import rootReducer from "./reducers/root";
 
 import "reset-css";
 import "./styles.css";
 
-import { BrowserRouter } from "react-router-dom";
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById("root")
 );
