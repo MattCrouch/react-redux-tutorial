4b1ea408dec17469cefc9a035ed14450a5381082
Matt Crouch <matt@mattcrouch.net>
2018-08-25 10:49:38 +0100

    Clear dummy data

--- src/components/container/Gallery/Gallery.js
+++ src/components/container/Gallery/Gallery.js
@@ -2,42 +2,9 @@ import React, { Component } from "react";
 
 import Gallery from "../../presentational/Gallery";
 
-const dummyPhotos = [
-  {
-    id: "1",
-    src: "gallery-photos/eiffel-tower.jpg",
-    comments: [
-      { id: "1", comment: "This is great", user: { id: "1", name: "Barry" } },
-      { id: "2", comment: "This is great", user: { id: "3", name: "Larry" } },
-      { id: "4", comment: "This is great", user: { id: "1", name: "Barry" } }
-    ],
-    user: { id: "1", name: "Barry" }
-  },
-  {
-    id: "2",
-    src: "gallery-photos/eiffel-tower.jpg",
-    comments: [
-      { id: "1", comment: "This is great", user: { id: "1", name: "Barry" } },
-      { id: "2", comment: "This is great", user: { id: "3", name: "Larry" } },
-      { id: "4", comment: "This is great", user: { id: "1", name: "Barry" } }
-    ],
-    user: { id: "1", name: "Barry" }
-  },
-  {
-    id: "3",
-    src: "gallery-photos/eiffel-tower.jpg",
-    comments: [
-      { id: "1", comment: "This is great", user: { id: "1", name: "Barry" } },
-      { id: "2", comment: "This is great", user: { id: "3", name: "Larry" } },
-      { id: "4", comment: "This is great", user: { id: "1", name: "Barry" } }
-    ],
-    user: { id: "1", name: "Barry" }
-  }
-];
-
 export class GalleryContainer extends Component {
   render() {
-    return <Gallery photos={dummyPhotos} />;
+    return <Gallery photos={[]} />;
   }
 }
 
--- src/components/container/Photo/Photo.js
+++ src/components/container/Photo/Photo.js
@@ -2,38 +2,9 @@ import React, { Component } from "react";
 
 import Photo from "../../presentational/Photo";
 
-const photo = {
-  id: "1",
-  src: "/gallery-photos/eiffel-tower.jpg",
-  comments: [
-    {
-      id: "1",
-      comment: "This is great",
-      top: 50,
-      left: 50,
-      user: { id: "1", name: "Barry" }
-    },
-    {
-      id: "2",
-      comment: "This is great",
-      top: 56,
-      left: 82,
-      user: { id: "3", name: "Larry" }
-    },
-    {
-      id: "4",
-      comment: "This is great",
-      top: 80,
-      left: 8,
-      user: { id: "1", name: "Barry" }
-    }
-  ],
-  user: { id: "1", name: "Barry" }
-};
-
 export class PhotoContainer extends Component {
   render() {
-    return <Photo id={photo.id} src={photo.src} comments={photo.comments} />;
+    return <Photo id="" src="" comments="" />;
   }
 }
 
