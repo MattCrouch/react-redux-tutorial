9148fff3896fbd848dc37f56ac984663b6661156
Matt Crouch <matt@mattcrouch.net>
2018-08-25 13:39:24 +0100

    Get photos

--- src/components/container/Gallery/Gallery.js
+++ src/components/container/Gallery/Gallery.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Gallery from "../../presentational/Gallery";
 
 export class GalleryContainer extends Component {
   render() {
-    return <Gallery photos={[]} />;
+    return <Gallery photos={this.props.photos} />;
   }
 }
 
-export default GalleryContainer;
+export const mapStateToProps = state => ({
+  photos: Object.values(state.photos)
+});
+
+export default connect(mapStateToProps)(GalleryContainer);
--- src/reducers/photos/photos.js
+++ src/reducers/photos/photos.js
@@ -1,4 +1,35 @@
-const initialState = {};
+const initialState = {
+  "1": {
+    id: "1",
+    src: "gallery-photos/eiffel-tower.jpg",
+    comments: [
+      { id: "1", comment: "This is great", user: { id: "1", name: "Barry" } },
+      { id: "2", comment: "This is great", user: { id: "3", name: "Larry" } },
+      { id: "4", comment: "This is great", user: { id: "1", name: "Barry" } }
+    ],
+    user: { id: "1", name: "Barry" }
+  },
+  "2": {
+    id: "2",
+    src: "gallery-photos/eiffel-tower.jpg",
+    comments: [
+      { id: "1", comment: "This is great", user: { id: "1", name: "Barry" } },
+      { id: "2", comment: "This is great", user: { id: "3", name: "Larry" } },
+      { id: "4", comment: "This is great", user: { id: "1", name: "Barry" } }
+    ],
+    user: { id: "1", name: "Barry" }
+  },
+  "3": {
+    id: "3",
+    src: "gallery-photos/eiffel-tower.jpg",
+    comments: [
+      { id: "1", comment: "This is great", user: { id: "1", name: "Barry" } },
+      { id: "2", comment: "This is great", user: { id: "3", name: "Larry" } },
+      { id: "4", comment: "This is great", user: { id: "1", name: "Barry" } }
+    ],
+    user: { id: "1", name: "Barry" }
+  }
+};
 
 export const reducer = (state = initialState, action) => {
   return state;
