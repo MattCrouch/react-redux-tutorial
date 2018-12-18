bf436cc5dbc37662341233ed54b7dc86f9c03453
Matt Crouch <matt@mattcrouch.net>
2018-12-18 15:25:09 +0000

    Fix compose

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { createStore, applyMiddleware } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import App from "./components/App";
 import rootReducer from "./reducers/root";
@@ -10,10 +10,10 @@ import rootReducer from "./reducers/root";
 import "reset-css";
 import "./styles.css";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
