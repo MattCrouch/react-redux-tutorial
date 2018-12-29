573e0d1cc69634f3214a40d66c040eb3a293e845
Matt Crouch <matt@mattcrouch.net>
2018-12-29 09:47:57 +0000

    Don't create a default user record for a photo

--- src/records/Photo.js
+++ src/records/Photo.js
@@ -1,13 +1,11 @@
 import { List, Record } from "immutable";
 
-import { UserRecord } from "./User";
-
 export const PhotoRecord = Record(
   {
     comments: List(),
     id: undefined,
     src: undefined,
-    user: UserRecord()
+    user: undefined
   },
   "Photo"
 );
